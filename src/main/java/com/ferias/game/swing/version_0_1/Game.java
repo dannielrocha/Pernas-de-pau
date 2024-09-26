3689321a8e1bd6ae3193845add29b79337e5eef0
danni@LAPTOP-DANNPC4 <danniel@ufpi.edu.br>
2024-09-26 06:22:00 -0300

    Corrigindo regra de negócio: objeto não deve desaparecer da tela.

--- src/main/java/com/ferias/game/swing/version_0_1/Game.java
+++ src/main/java/com/ferias/game/swing/version_0_1/Game.java
@@ -9,32 +9,28 @@ public class Game {
 	
 	public Game(Dimension dimension) {
 		this.dimensao = dimension;
-		colorify();
+		colorir();
 	}
 	
 	public void horizontalMove(int step){
-		if (step < 0 && (bola.getX() > 0))
-			bola.horizontalMove(step);
-		else if (((bola.getX() + 10) < dimensao.getWidth()))
+		if ( (bola.getX() >= 10 && step < 0) || (step > 0 && bola.getX()+10 <= dimensao.getWidth()-10) )
 			bola.horizontalMove(step);
 		
-		colorify();
+		colorir();
 	}
 	
 	public void verticalMove(int step){
-		if (step < 0 && bola.getY() > 0)
-			bola.verticalMove(step);
-		else if ((bola.getY()+10) < dimensao.getHeight())
+		if ( (bola.getY() >= 10 && step < 0) || (step > 0 && bola.getY()+10 <= dimensao.getHeight()-10) )
 			bola.verticalMove(step);
 		
-		colorify();
+		colorir();
 	}
 	
 	public Ball getBola() {
 		return this.bola;
 	}
 	
-	private final void colorify() {
+	private final void colorir() {
 		if (bola.getY() > dimensao.getHeight()/2 && bola.getX() < dimensao.getWidth()/2)
 			bola.setColor(Color.BLUE);
 		else if (bola.getY() < dimensao.getHeight()/2 && bola.getX() < dimensao.getWidth()/2)
