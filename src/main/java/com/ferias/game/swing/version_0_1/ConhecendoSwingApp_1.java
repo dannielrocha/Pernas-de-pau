05ff248d10fa22b52e72fd28aeb4a08c6c285b36
danni@LAPTOP-DANNPC4 <danniel@ufpi.edu.br>
2024-10-21 13:33:20 -0300

    Adicionando mais exemplos

--- src/main/java/com/ferias/game/swing/version_0_1/ConhecendoSwingApp_1.java
+++ src/main/java/com/ferias/game/swing/version_0_1/ConhecendoSwingApp_1.java
@@ -3,10 +3,12 @@ package com.ferias.game.swing.version_0_1;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -17,16 +19,24 @@ import javax.swing.JPanel;
 public class ConhecendoSwingApp_1 {
 	public static void main(String[] args) {
 
+		/*Cria uma nova janela com o título 'Simple Swing App'.*/
 		JFrame frontView = new JFrame("Simple Swing App");
-
+		/*Define se a janela pode ser sobreposta por outras.*/
 		frontView.setAlwaysOnTop(true);
+		/*Define se a janela pode ser redimensionada pelo usuário.*/
+		frontView.setResizable(false);
+		/*Define o comportamento padrão para o fechamento da janela.*/
 		frontView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/*Define se a janela requisitará foco para si logo que for exibida.*/
 		frontView.setAutoRequestFocus(true);
+		/*Define um algoritmo de organização automática dos componentes.*/
 		frontView.setLayout(new BorderLayout());
-		TestPane gamePanel = new TestPane();
-		frontView.add(gamePanel);
+		/*Define um componente relativo ao qual o centro da janela será posicionado.*/
 		frontView.setLocationRelativeTo(null);
 
+		TestPane painel = new TestPane();
+		frontView.getContentPane().add(painel);
+		
 		// pede para o usuário inserir seu nome
 		String name = JOptionPane.showInputDialog("Digite seu nome:");
 		// cria a mensagem
@@ -35,7 +45,9 @@ public class ConhecendoSwingApp_1 {
 		// exibe a mensagem para cumprimentar o usuário pelo nome
 		JOptionPane.showMessageDialog(frontView, message);
 
+		/*Redimensiona a janela com base nas propriedades preferredSize de seus subcomponentes.*/
 		frontView.pack();
+		/*Torna a janela visível para o usuário.*/
 		frontView.setVisible(true);
 
 		frontView.addKeyListener(new KeyAdapter() {
@@ -55,7 +67,7 @@ public class ConhecendoSwingApp_1 {
 	}
 }
 
-//Innerclass for convinience of this exercise
+
 class TestPane extends JPanel {
 	private static final long serialVersionUID = 1L;
 
@@ -68,6 +80,23 @@ class TestPane extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.GREEN);
+		g.fillArc(this.getWidth()/2, 250, 30, 30, 135, 180);
 		g.fillOval(200, 100, 30, 30);
+		g.fillRect(100, 200, 30, 30);
+		g.fillRoundRect(30, 30, 25, 25, 20, 20);
+		g.fill3DRect(40, 50, 40, 40, true);
+		g.clearRect(100, 200, 20, 20);
+		
+		g.setColor(Color.BLUE);
+		g.drawLine(0, this.getHeight()/2, this.getWidth()/2, this.getHeight());
+		g.drawOval(150, 100, 30, 30);
+		g.drawRect(150, 200, 30, 30);
+		g.drawRoundRect(70, 30, 25, 25, 20, 20);
+		g.setFont(new Font("Arial Bold", Font.PLAIN, 14));
+		g.drawString("Um texto pode ser facilmente adicionado", 10, 20);
+		g.draw3DRect(70, 50, 40, 40, true);
+		
+		ImageIcon image = new ImageIcon("src/resources/duke.png");
+		g.drawImage(image.getImage(), 60, 110, 137, 80, null, null);
 	}
 }
--- src/main/java/com/ferias/game/swing/version_0_1/ConhecendoSwingApp_2.java
+++ src/main/java/com/ferias/game/swing/version_0_1/ConhecendoSwingApp_2.java
@@ -11,52 +11,71 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
  * 
  */
 public class ConhecendoSwingApp_2 {
+	
 	public static void main(String[] args) {
-
-		JFrame frontView = new JFrame("Simple Swing App");
-
-		frontView.setAlwaysOnTop(true);
-		frontView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frontView.setAutoRequestFocus(true);
-		frontView.setLayout(new BorderLayout());
-		GamePanel gamePane = new GamePanel();
-		frontView.add(gamePane);
-		frontView.pack();
-		frontView.setLocationRelativeTo(null);
 		
-		frontView.setVisible(true);
+		SwingUtilities.invokeLater(new Runnable() {
 
-		frontView.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				int keyCode = e.getKeyCode();
-				if (keyCode == KeyEvent.VK_UP) {
-					System.out.println("Up Arrrow-Key is pressed!");
-					gamePane.verticalMove(-10);
-				} else if (keyCode == KeyEvent.VK_DOWN) {
-					System.out.println("Down Arrrow-Key is pressed!");
-					gamePane.verticalMove(10);
-				} else if (keyCode == KeyEvent.VK_LEFT) {
-					System.out.println("Left Arrrow-Key is pressed!");
-					gamePane.horizontalMove(-10);
-				} else if (keyCode == KeyEvent.VK_RIGHT) {
-					System.out.println("Right Arrrow-Key is pressed!");
-					gamePane.horizontalMove(10);
+
+			@Override
+			public void run() {
+				try {
+					/*
+					 * Tenta selecionar um modelo de aparência para as janelas baseado no sistema
+					 * operacional.
+					 */
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (Exception ex) {
+					ex.printStackTrace();
 				}
-				gamePane.repaint();
-			}
-		});
-
-		frontView.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
-
+				JFrame frontView = new JFrame("Simple Swing App");
+		
+				frontView.setAlwaysOnTop(true);
+				frontView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frontView.setAutoRequestFocus(true);
+				frontView.setLayout(new BorderLayout());
+				GamePanel gamePane = new GamePanel();
+				frontView.add(gamePane);
+				frontView.pack();
+				frontView.setLocationRelativeTo(null);
+				
+				frontView.setVisible(true);
+		
+				frontView.addKeyListener(new KeyAdapter() {
+					public void keyPressed(KeyEvent e) {
+						int keyCode = e.getKeyCode();
+						if (keyCode == KeyEvent.VK_UP) {
+							System.out.println("Up Arrrow-Key is pressed!");
+							gamePane.verticalMove(-10);
+						} else if (keyCode == KeyEvent.VK_DOWN) {
+							System.out.println("Down Arrrow-Key is pressed!");
+							gamePane.verticalMove(10);
+						} else if (keyCode == KeyEvent.VK_LEFT) {
+							System.out.println("Left Arrrow-Key is pressed!");
+							gamePane.horizontalMove(-10);
+						} else if (keyCode == KeyEvent.VK_RIGHT) {
+							System.out.println("Right Arrrow-Key is pressed!");
+							gamePane.horizontalMove(10);
+						}
+						gamePane.repaint();
+					}
+				});
+		
+				frontView.addWindowListener(new WindowAdapter() {
+					public void windowClosing(WindowEvent e) {
+						System.exit(0);
+					}
+				});
+		
+			}//fim: run(){
+		}/*fim: new Runnable() { */);//fim: SwingUtilities.invokeLater(
 	}
 }
 
@@ -93,4 +112,5 @@ class GamePanel extends JPanel {
 	public void verticalMove(int yPos) {
 		this.yPos += yPos;
 	}
+
 }
